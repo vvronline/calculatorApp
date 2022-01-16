5ed356441ee4facdee48cdb2e060e9d4d541a6af
vvronline <vishnuvr19@gmail.com>
2022-01-17 00:58:36 +0530

    Added backspace button and fixed undefined error

--- script.js
+++ script.js
@@ -5,16 +5,24 @@ for (item of buttons) {
   item.addEventListener("click", (e) => {
     buttonText = e.target.innerText;
     //console.log("Button text is ", buttonText);
+    if (buttonText == "=" && screenValue == "") {
+      screenValue += "Press Any Keys";
+      screen.value = screenValue;
+    }
 
     if (buttonText == "x") {
       buttonText = "*";
       screenValue += buttonText;
       screen.value = screenValue;
     } else if (buttonText == "AC") {
-      screenValue = " ";
+      screenValue = "";
       screen.value = screenValue;
     } else if (buttonText == "=") {
       screen.value = eval(screenValue);
+    } else if (buttonText == "Del") {
+      let res = screenValue.toString();
+      screenValue = res.slice(0, res.length - 1);
+      screen.value = screenValue;
     } else {
       screenValue += buttonText;
       screen.value = screenValue;
